af4aa70eebba77e86cf095b766a4e5562a0ff5aa
Amanda Troxel <amandatroxel2@gmail.com>
2021-07-06 10:26:57 -0600

    update

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("assets/data/data.csv").then(function(stateData) {
+    var healthcare = stateData.map(data => data.healthcare);
+    var poverty = stateData.map(data => data.poverty);
+})
