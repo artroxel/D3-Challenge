a5af6b2143b9c59f6f34c9320c81c5c498bb8212
Amanda Troxel <amandatroxel2@gmail.com>
2021-07-10 09:10:45 -0600

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,11 +26,11 @@ d3.csv("assets/data/data.csv").then(function(stateData) {
     });
 
     var xLinearScale = d3.scaleLinear()
-    .domain([20, d3.max(stateData, d => d.poverty)])
+    .domain([d3.min(stateData, d => d.poverty), d3.max(stateData, d => d.poverty)])
     .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(stateData, d => d.healthcare)])
+    .domain([d3.min(stateData, d => d.healthcare), d3.max(stateData, d => d.healthcare)])
     .range([height, 0]);
 
     var bottomAxis = d3.axisBottom(xLinearScale);
@@ -46,10 +46,46 @@ d3.csv("assets/data/data.csv").then(function(stateData) {
     var circlesGroup = chartGroup.selectAll("circle")
         .data(stateData)
         .enter()
+
+        circlesGroup
         .append("circle")
         .attr("cx", d => xLinearScale(d.poverty))
         .attr("cy", d=> yLinearScale(d.healthcare))
         .attr("r", "10")
         .attr("fill", "blue")
         .attr("opacity", ".5");
+    console.log(stateData)
+    // var textGroup = chartGroup.selectAll("text")
+    //     .data(stateData)
+    //     .enter()
+    //     .append("text")
+    //     .text(function(d){ return d.abbr})
+    //     .attr("dx", d => xLinearScale(d.poverty))
+    //     .attr("dy", d=> yLinearScale(d.healthcare)+10/2.5)
+    //     .attr("font-size", "10")
+    //     .attr("fill", "red")
+    //     .attr("text-anchor", "middle")
+   circlesGroup
+    .append("text")
+    .text(function(d){ return d.abbr})
+    .attr("dx", d => xLinearScale(d.poverty))
+    .attr("dy", d=> yLinearScale(d.healthcare)+10/2.5)
+    .attr("font-size", "10")
+    .attr("fill", "white")
+    .attr("text-anchor", "middle")
+
+    chartGroup
+    .append("text")
+    .attr("text-anchor", "end")
+    .attr("x", svgWidth/2)
+    .attr("y", svgHeight -50)
+    .text("Poverty %")
+
+    chartGroup
+    .append("text")
+    .attr("text-anchor", "end")
+    .attr("y", -40)
+    .attr("dy", ".5em")
+    .attr("transform", "rotate(-90)")
+    .text("Lacks Healthcare %")
 })
