3e1a25641fd4bd458fd1a36140c30871987a48b8
Amanda Troxel <amandatroxel2@gmail.com>
2021-07-09 19:40:04 -0600

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,6 +20,36 @@ var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.csv("assets/data/data.csv").then(function(stateData) {
-    var healthcare = stateData.map(data => data.healthcare);
-    var poverty = stateData.map(data => data.poverty);
+    stateData.forEach(function(data) {
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+    .domain([20, d3.max(stateData, d => d.poverty)])
+    .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(stateData, d => d.healthcare)])
+    .range([height, 0]);
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(stateData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d=> yLinearScale(d.healthcare))
+        .attr("r", "10")
+        .attr("fill", "blue")
+        .attr("opacity", ".5");
 })
